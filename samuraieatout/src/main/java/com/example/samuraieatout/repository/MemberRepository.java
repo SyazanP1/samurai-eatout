e615ea1756312cdef5efd11357aa180911db6557
SyazanP1 <nakanaka100@outlook.jp>
2025-05-20 08:15:48 +0900

    管理者用会員一覧、検索機能を追加

--- samuraieatout/src/main/java/com/example/samuraieatout/repository/MemberRepository.java
+++ samuraieatout/src/main/java/com/example/samuraieatout/repository/MemberRepository.java
@@ -1,10 +1,16 @@
 package com.example.samuraieatout.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.samuraieatout.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer>{
 
-	public Member findByEmail(String email);
+	Member findByEmail(String email);
+	
+	Page<Member> findAllByOrderByIdAsc(Pageable pageable);
+	
+	Page<Member> findByNameLikeOrderByIdAsc(String name, Pageable pageable);
 }
